fc7113494d0713d8f861a947cbe0ec3cd7695b9b
discotechnology <drosenfeld@csumb.edu>
2025-11-23 15:09:28 -0800

    More tweaks to styles - site complete

--- index.mjs
+++ index.mjs
@@ -8,12 +8,16 @@ app.use(express.static("public"));
 app.get('/', async(req, res) => {
    let apiKey = "7756a1e81f817c186cf57294e1c19b37b49c54b8f34e7c499ee0ce5cd86cd16e";
    let url = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&featured=true&query=solar-system`;
-   let response = await fetch(url);
-   let data = await response.json();
-   let randomImage = data.urls.full;
+   var randomImage = "/img/solar-system.jpg"
+   try {
+      let response = await fetch(url);
+      let data = await response.json();
+      randomImage = data.urls.full;
+   } catch (e) {
+      console.log(e);
+   }
    res.render("index",{"image":randomImage})
 });
-
 
 app.get('/earth', (req, res) => {
    let planet = planets.getEarth();
@@ -69,7 +73,6 @@ app.get('/nasa', async(req, res) => {
    let nasaFacts = await response.json();
    res.render("nasa",{nasaFacts})
 });
-
 
 app.listen(3000, () => {
    console.log('server started');
