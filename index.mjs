b1cf1d5d583aeeef19dc33463db642e2ac045e92
discotechnology <drosenfeld@csumb.edu>
2025-11-23 13:59:08 -0800

    Built routes for remaining planets, fixed images of Jupiter and Uranus

--- index.mjs
+++ index.mjs
@@ -21,6 +21,42 @@ app.get('/mars', (req, res) => {
    res.render('planet', {"planet":planet, "planetName":"Mars"});
 });
 
+app.get('/mercury', (req, res) => {
+   let planet = planets.getMercury();
+   console.log(planet);
+   res.render('planet', {"planet":planet, "planetName":"Mercury"});
+});
+
+app.get('/venus', (req, res) => {
+   let planet = planets.getVenus();
+   console.log(planet);
+   res.render('planet', {"planet":planet, "planetName":"Venus"});
+});
+
+app.get('/jupiter', (req, res) => {
+   let planet = planets.getJupiter();
+   console.log(planet);
+   res.render('planet', {"planet":planet, "planetName":"Jupiter"});
+});
+
+app.get('/saturn', (req, res) => {
+   let planet = planets.getSaturn();
+   console.log(planet);
+   res.render('planet', {"planet":planet, "planetName":"Saturn"});
+});
+
+app.get('/uranus', (req, res) => {
+   let planet = planets.getUranus();
+   console.log(planet);
+   res.render('planet', {"planet":planet, "planetName":"Uranus"});
+});
+
+app.get('/neptune', (req, res) => {
+   let planet = planets.getNeptune();
+   console.log(planet);
+   res.render('planet', {"planet":planet, "planetName":"Neptune"});
+});
+
 
 app.listen(3000, () => {
    console.log('server started');
