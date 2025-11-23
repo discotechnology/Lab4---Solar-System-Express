8170def6fbd5bc32fd497e797acb05ac16681a42
discotechnology <drosenfeld@csumb.edu>
2025-11-23 12:57:37 -0800

    Added 'earth' route

--- index.mjs
+++ index.mjs
@@ -7,6 +7,11 @@ app.get('/', (req, res) => {
    res.render('index');
 });
 
+app.get('/earth', (req, res) => {
+ res.render('earth')
+});
+
+
 app.listen(3000, () => {
    console.log('server started');
 });
